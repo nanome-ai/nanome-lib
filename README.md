4b6e00e381ae324eb53941a808dea76587fb6f83
Ramji <jeremie.rahm@epitech.eu>
2019-04-20 20:36:06 -0700

    Add files required by PyPi

---
+++ README.md
@@ -0,0 +1,24 @@
+# Nanome
+
+This library allows users to create powerful plugins for Nanome (https://nanome.ai/), using a simple Python API.
+
+### Examples
+
+*Coming soon*
+
+### Installation
+
+In order to use Nanome Plugin API, you need Python 3.5+ or 2.7+
+To install the library, run:
+
+```sh
+$ pip install nanome
+```
+
+### Documentation
+
+All documentation needed to use this package is available at [Read The Docs](https://nanome.readthedocs.io/en/latest/)
+
+### License
+
+MIT
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+	name = 'nanome',
+	packages = ['nanome'],
+	version = '0.1',
+	license='MIT',
+	description = 'Python API for Nanome Plugins',
+	author = 'Nanome',
+	author_email = 'hello@nanome.ai',
+	url = 'https://github.com/nanome-ai/nanome',
+	download_url = 'https://github.com/nanome-ai/nanome/archive/v0.0.1.tar.gz',
+	keywords = ['virtual-reality', 'chemistry', 'python', 'api', 'plugin'],
+	install_requires=[],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering :: Chemistry',
+
+		'License :: OSI Approved :: MIT License',
+
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+	],
+)
