d9f22ce57cf2555d02dc2f78b6bd0f5828f893e3
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2019-05-01 13:38:51 -0700

    pulled over the testplugin fix.

--- TestPlugin.py
+++ TestPlugin.py
@@ -1,38 +1,22 @@
 import nanome
 from timeit import default_timer as timer
 
-
-class TestPlugin(nanome.PluginInstance):
-    def __init__(self):
-        self._state = 0
-
-    def update(self):
-        end = timer()
-        if end - self._start_timer >= 3:
-            if self._state == 0:
-                self.set_plugin_list_button(nanome.PluginInstance.PluginListButtonType.run, "Nope", False)
-                self.set_plugin_list_button(nanome.PluginInstance.PluginListButtonType.advanced_settings, "Nope", False)
-            elif self._state == 1:
-                self.set_plugin_list_button(nanome.PluginInstance.PluginListButtonType.run, None, True)
-                self.set_plugin_list_button(nanome.PluginInstance.PluginListButtonType.advanced_settings, None, True)
-            else:
-                self.set_plugin_list_button(nanome.PluginInstance.PluginListButtonType.run, "Run", True)
-                self.set_plugin_list_button(nanome.PluginInstance.PluginListButtonType.advanced_settings, "Advanced Settings", True)
-            self._state = (self._state + 1) % 3
-            self._start_timer = timer()
-
-    def start(self):
-        # Access the API to add a button on modify menu, name Split, section Editing, and sending event MenuButtonClicked
-        nanome.util.Logs.debug("Start TestPlugin")
-        self._start_timer = timer()
-
 import sys, inspect
 import test_plugins
 if __name__ == "__main__":
-    plugin_name = "test_plugins." + sys.argv[1]
+    class_name = sys.argv[1]
+    module_name = "test_plugins." + class_name
     del sys.argv[1]
-    clsmembers = inspect.getmembers(sys.modules[plugin_name], inspect.isclass)
-    plugin_class = clsmembers[0][0]
+    if module_name not in sys.modules:
+        raise "No module: " + module_name
+    clsmembers = inspect.getmembers(sys.modules[module_name], inspect.isclass)
+    plugin_class = None
+    for clsmember in clsmembers:
+        if (clsmember[0] == class_name):
+            plugin_class = clsmember[1]
+            break
+    if plugin_class == None:
+        raise "No class " + class_name + " in module " + module_name
     plugin = nanome.Plugin("Testing Plugin", "Plugin is being run using the tester plugin.", "Test", True)
     plugin.set_plugin_class(plugin_class)
     plugin.run('127.0.0.1', 8888)
