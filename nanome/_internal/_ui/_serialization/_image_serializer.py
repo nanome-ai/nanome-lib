2530069d2d203b742cf899aadffd73c54428cee6
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2019-05-02 16:06:19 -0700

    moved the image serializer stuff a bit.

--- nanome/_internal/_ui/_serialization/_image_serializer.py
+++ nanome/_internal/_ui/_serialization/_image_serializer.py
@@ -19,20 +19,21 @@ class _ImageSerializer(_TypeSerializer):
     def serialize(self, version, value, context):
         context.write_int(value._content_id)
         context.write_using_serializer(self.string, value._file_path)
+        context.write_using_serializer(self.color, value._color)
+        context.write_uint(value._scaling_option)
         data = []
         if (value._file_path != ""):
             with open(value._file_path, "rb") as f:
                 data = f.read()
         context.write_using_serializer(self.data, data)
-        context.write_using_serializer(self.color, value._color)
-        context.write_uint(value._scaling_option)
 
     def deserialize(self, version, context):
         value = _Image._create()
         value._content_id = context.read_int()
-        context.read_using_serializer(self.data) #skipping data.
+        value._file_path = context.read_using_serializrt(self.string)
         value._color = context.read_using_serializer(self.color)
         value._scaling_option = context.read_uint()
+        context.read_using_serializer(self.data) #skipping data.
         return value
 
 _UIBaseSerializer.register_type("Image", _UIBaseSerializer.ContentType.eimage, _ImageSerializer())
