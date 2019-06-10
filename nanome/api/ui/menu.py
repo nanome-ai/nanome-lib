38a41e72c912d80f670fb8ab83e0bd579e1f873d
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2019-06-10 14:21:58 -0700

    Revert "enabled -> _enabled"
    This reverts commit 908644cbfb74b4aaf994754b110b60a12dd61434.

--- nanome/api/ui/menu.py
+++ nanome/api/ui/menu.py
@@ -15,30 +15,6 @@ class Menu(_Menu):
 
     #region properties
     @property
-    def enabled(self):
-        return self._enabled
-    
-    @enabled.setter
-    def enabled(self, value):
-        self._enabled = value
-
-    @property
-    def title(self):
-        return self._title
-    
-    @title.setter
-    def title(self, value):
-        self._title = value
-
-    @property
-    def locked(self):
-        return self._locked
-    
-    @locked.setter
-    def locked(self, value):
-        self._locked = value
-
-    @property
     def root(self):
         return self._root
     
