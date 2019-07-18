8653bec20ba56d86e12a7ac51846ef94eb562f6a
Ethan Vander Horn <Ethan.vanderh123@gmail.com>
2019-07-18 09:55:40 -0700

    Added functions for quaternion manipulation (#53)

--- nanome/util/quaternion.py
+++ nanome/util/quaternion.py
@@ -39,6 +39,55 @@ class Quaternion(object):
         self._z *= -1.0
         return self
 
+    def get_conjugate(self):
+        return Quaternion(-self.x, -self.y, -self.z, self.w)
+
     @classmethod
     def _get_inversed_handedness(cls, value):
         return Quaternion(value.x, -value.y, -value.z, value.w)
+
+    def __mul__(self, other):
+        q =self
+        if isinstance(other, Quaternion):
+            r = other
+            w = r.w * q.w - r.x * q.x - r.y * q.y - r.z * q.z
+            x = r.w * q.x + r.x * q.w - r.y * q.z + r.z * q.y
+            y = r.w * q.y + r.x * q.z + r.y * q.w - r.z * q.x
+            z = r.w * q.z - r.x * q.y + r.y * q.x + r.z * q.w
+            return Quaternion(x,y,z,w)
+        elif (isinstance(other, float) or isinstance(other, int)):
+            n = other
+            return Quaternion(q.x * n, q.y * n, q.z * n, q.w * n)
+        else:
+            raise NotImplementedError
+
+    def rotate_vector(self, point):
+        r = Quaternion(point.x, point.y, point.z, 0)
+        q_conj = self.get_conjugate()
+        result = ((self * r) * q_conj)
+        point = point.get_copy()
+        point.x = result.x
+        point.y = result.y
+        point.z = result.z
+        return point
+
+    #algorithm credit https://d3cw3dd2w32x2b.cloudfront.net/wp-content/uploads/2015/01/matrix-to-quat.pdf
+    @classmethod
+    def from_matrix(cls, matrix):
+        m = matrix
+        if (m[2][2] < 0):
+            if (m[0][0] > m[1][1]):
+                t = 1 + m[0][0] - m[1][1] - m[2][2]
+                q = cls( t, m[1][0]+m[0][1], m[0][2]+m[2][0], m[2][1]-m[1][2] )
+            else:
+                t = 1 - m[0][0] + m[1][1] - m[2][2]
+                q = cls( m[1][0]+m[0][1], t, m[2][1]+m[1][2], m[0][2]-m[2][0] )
+        else:
+            if (m[0][0] < -m[1][1]):
+                t = 1 - m[0][0] - m[1][1] + m[2][2]
+                q = cls( m[0][2]+m[2][0], m[2][1]+m[1][2], t, m[1][0]-m[0][1] )
+            else:
+                t = 1 + m[0][0] + m[1][1] + m[2][2]
+                q = cls( m[2][1]-m[1][2], m[0][2]-m[2][0], m[1][0]-m[0][1], t )
+        q *= 0.5 / math.sqrt(t)
+        return q
