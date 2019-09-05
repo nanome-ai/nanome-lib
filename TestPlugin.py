aba67009e21463c42357d1b01f4f4ea2d9f50b09
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2019-09-05 16:01:22 -0700

    added an "all" option to testplugin

--- TestPlugin.py
+++ TestPlugin.py
@@ -1,6 +1,28 @@
-import sys
+import sys, multiprocessing, time, importlib
+
+def launch_plugin(class_name, args):
+    module_name = "test_plugins." + class_name
+    import nanome
+    sys.argv = args
+    module = importlib.import_module(module_name)
+    plugin_class = importlib.import_module(module_name, class_name)
+    plugin = nanome.Plugin(module.NAME, module.DESCRIPTION, module.CATEGORY, module.HAS_ADVANCED_OPTIONS)
+    plugin.set_plugin_class(plugin_class)
+    plugin.run("config", "config", "config")
+
 if __name__ == "__main__":
     class_name = sys.argv[1]
-    module_name = "test_plugins." + class_name
     del sys.argv[1]
-    new_module = __import__(module_name)
+    if class_name.lower() == "all":
+        class_names = []
+        from os.path import dirname, basename, isfile, join
+        import glob
+        modules = glob.glob(join(dirname(__file__), join("test_plugins", "*.py")))
+        class_names = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+        for class_name in class_names:
+            x = multiprocessing.Process(target=launch_plugin, args = (class_name, sys.argv))
+            x.start()
+        while(True):
+            time.sleep(1)
+    else:
+        importlib.import_module("test_plugins." + class_name)
