be710b2fa847397686543d150ccb6acd26db4699
Ramji <jeremie@nanome.ai>
2019-12-12 22:18:37 +0100

    Fix convenience check

--- nanome/api/plugin_instance.py
+++ nanome/api/plugin_instance.py
@@ -391,7 +391,7 @@ class PluginInstance(_PluginInstance):
 
     def send_files_to_load(self, files_list, callback = None):
         files = []
-        if not files_list is list:
+        if not isinstance(files_list, list):
             files_list = [files_list]
         for file in files_list:
             full_path = file.replace('\\', '/')
