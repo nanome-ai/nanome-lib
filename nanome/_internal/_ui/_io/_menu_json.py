0010be62096d03e00c32bb1fdfdedc29f7f307bb
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2020-01-16 12:27:29 -0800

    fixed menu defaults.

--- nanome/_internal/_ui/_io/_menu_json.py
+++ nanome/_internal/_ui/_io/_menu_json.py
@@ -6,9 +6,9 @@ import json
 def parse_json(menu_json):
     new_menu = _Menu._create()
     assert(menu_json.read("is_menu", True))
-    new_menu._title = menu_json.read("title", "menu")
-    new_menu._width = menu_json.read("width", 0.0)
-    new_menu._height = menu_json.read("height", 0.0)
+    new_menu._title = menu_json.read("title", new_menu._title)
+    new_menu._width = menu_json.read("width", new_menu._width)
+    new_menu._height = menu_json.read("height", new_menu._height)
     root = menu_json.read_object("effective_root")
     new_menu._root = _layout_node_json.parse_json(root)
     return new_menu
