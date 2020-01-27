1387269181190cbcaa64ec959d9923812ea4b7cd
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2020-01-27 14:10:00 -0800

    int->byte for index

--- nanome/_internal/_network/_commands/_serialization/_menu_callback.py
+++ nanome/_internal/_network/_commands/_serialization/_menu_callback.py
@@ -12,12 +12,12 @@ class _MenuCallback(_TypeSerializer):
         
     def serialize(self, version, value, context):
         if version >= 1:
-            context.write_int(value[0])
+            context.write_byte(value[0])
         context.write_bool(value[1])
 
     def deserialize(self, version, context):
         if version >= 1:
-            index = context.read_int()
+            index = context.read_byte()
         else:
             index = 0
         value = context.read_bool()
