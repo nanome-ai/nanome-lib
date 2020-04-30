e17e2adc8fff0b718c18c3b6644e402502c57c76
Ramjii <jeremie@nanome.ai>
2020-04-30 14:37:04 -0700

    Revert "I lost all hope"
    This reverts commit 278d9f1052c95266795e163f777c23cdac8d5f2c.

--- nanome/_internal/__init__.py
+++ nanome/_internal/__init__.py
@@ -1,4 +1,8 @@
 from . import *
+#classes
+from ._addon import _Addon
+from ._plugin_instance import _PluginInstance
+
 #folders
 from . import _util
 from . import _structure
@@ -6,7 +10,5 @@ from . import _ui
 from . import _network
 from . import _volumetric
 from . import _macro
-#classes
-from ._addon import _Addon
-from ._plugin_instance import _PluginInstance
+
 from ._plugin import _Plugin
