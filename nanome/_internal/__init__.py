278d9f1052c95266795e163f777c23cdac8d5f2c
Ramjii <jeremie@nanome.ai>
2020-04-30 14:26:53 -0700

    I lost all hope

--- nanome/_internal/__init__.py
+++ nanome/_internal/__init__.py
@@ -1,8 +1,4 @@
 from . import *
-#classes
-from ._addon import _Addon
-from ._plugin_instance import _PluginInstance
-
 #folders
 from . import _util
 from . import _structure
@@ -10,5 +6,7 @@ from . import _ui
 from . import _network
 from . import _volumetric
 from . import _macro
-
+#classes
+from ._addon import _Addon
+from ._plugin_instance import _PluginInstance
 from ._plugin import _Plugin
