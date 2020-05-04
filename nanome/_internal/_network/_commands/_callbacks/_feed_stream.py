e69b64794f1531673575cae580b775c8a668334b
Jeremie Rahm <Ramjii@users.noreply.github.com>
2020-05-04 09:33:35 -0700

    Fix exec log to file (#126)
    * a
    
    * a

--- nanome/_internal/_network/_commands/_callbacks/_feed_stream.py
+++ nanome/_internal/_network/_commands/_callbacks/_feed_stream.py
@@ -1,4 +1,3 @@
-from nanome.api.streams import Stream
-
 def _feed_stream(network, result, request_id):
+    from nanome.api.streams import Stream
     Stream._streams[result[0]]._update_received(result[1])
