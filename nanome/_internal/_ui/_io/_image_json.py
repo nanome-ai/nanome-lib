6b8a47b81460eb29b26856d877f15877803f9ed8
Alex McNerney <ajm13@me.com>
2020-07-20 15:41:18 -0700

    Fix error from unneeded "if" in _image_json

--- nanome/_internal/_ui/_io/_image_json.py
+++ nanome/_internal/_ui/_io/_image_json.py
@@ -5,8 +5,7 @@ def parse_json(content_json):
     image = _Image._create()
     image._color = content_json.read("color", image._color)
     image._file_path = content_json.read("file_path", image._file_path)
-    if ("scaling_option" in content_json):
-        image._scaling_option = _Image.ScalingOptions(content_json.read("scaling_option", image._scaling_option))
+    image._scaling_option = _Image.ScalingOptions(content_json.read("scaling_option", image._scaling_option))
     return image
 
 def write_json(helper, image):
