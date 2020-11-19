56a7a8cbbb57341b74e712b7b6b868609206ff59
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2020-11-18 17:54:54 -0800

    trying to figure out the doc string

--- nanome/_internal/_util/_overload.py
+++ nanome/_internal/_util/_overload.py
@@ -1,30 +1,28 @@
 import inspect
-
+from functools import wraps
 
 class overload (object):
     def __get__(self, instance, owner):
         self.obj = instance
-        print("Agent %s called from %s " % (id(self), instance.name))
         return self
 
     def __init__(self, f):
         self.cases = {}
-        self.alternate(f)
+        signature = inspect.getargspec(f).args
+        self.cases[len(signature)] = f
+        self.f = f
 
-    def alternate(self, g):
-        print(g)
-        signature = inspect.getargspec(g).args
-        self.cases[len(signature)] = g
-        print(len(signature))
-        print(self.cases)
-        return self
+    @property
+    def alternate(self):
+        @wraps(self.f)
+        def store_function(f):
+            signature = inspect.getargspec(f).args
+            self.cases[len(signature)] = f
+            return self
+        return store_function
 
     def __call__(self, *args):
-        print(self.cases)
-        print(args)
-        print(self)
         function = self.cases[len(args) + 1]
-        print(function)
         return function(self.obj, *args)
 
 
@@ -33,13 +31,6 @@ class Files(object):
         self.name = "fred"
 
     @overload
-    def ls(self, s):
-        '''
-         test
-        '''
-        print("1 arg version")
-
-    @ls.alternate
     def ls(self, path, pattern):
         '''
          test test dude it woooorks.
@@ -47,10 +38,15 @@ class Files(object):
         '''
         print("2 arg version")
 
+    @ls.alternate
+    def ls(self, s):
+        '''
+         test
+        '''
+        print("1 arg version")
 
 f = Files()
 
 print("--------------testing-----------------")
 f.ls("s")
 f.ls("s", "t")
-f.ls()
