906474fcc9615124cbbf95d36ea883f807712287
Max Howard <max@nanome.ai>
2021-02-17 11:20:48 -0800

    optimize nanome-source command

--- plugin-template/.devcontainer/.bashrc.sh
+++ plugin-template/.devcontainer/.bashrc.sh
@@ -15,19 +15,17 @@ pip-save() {
 }
 
 nanome-source() {
-  wd=$(pwd)
-  cd .. && git clone https://github.com/nanome-ai/nanome-lib.git && cd nanome-lib
-  if [[ $1 == latest ]]; then
-    branch=$(git describe --tags `git rev-list --tags --max-count=1`)
-  elif [[ $1 != "" ]]; then
-    branch=$1
-  else
-    branch=master
-  fi
-  git checkout $branch
-  (cd plugin-template && zip -9r ../nanome/plugin-template.zip .)
-  pip install --upgrade . && cd $wd
-  rm -rf ../nanome-lib
+  (
+    cd .. && git clone https://github.com/nanome-ai/nanome-lib.git && cd nanome-lib &&\
+    if [[ $1 == latest ]]; then
+      git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+    elif [[ $1 != "" ]]; then
+      git checkout $1
+    fi &&\
+    (cd plugin-template && zip -9r ../nanome/plugin-template.zip .) &&\
+    pip install --upgrade . &&\
+    rm -rf ../nanome-lib
+  )
 }
 
 function dipop() {
