4d54eef61607bcac567723678e384ce1da164710
Max Howard <max@nanome.ai>
2021-02-17 14:53:48 -0800

    simplify nanome-source

--- plugin-template/.devcontainer/.bashrc.sh
+++ plugin-template/.devcontainer/.bashrc.sh
@@ -17,13 +17,10 @@ pip-save() {
 nanome-source() {(
   cd .. && git clone https://github.com/nanome-ai/nanome-lib.git && cd nanome-lib &&\
   if [[ $1 == latest ]]; then
-    branch=$(git describe --tags `git rev-list --tags --max-count=1`)
+    git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
   elif [[ $1 != "" ]]; then
-    branch=$1
-  else
-    branch=master
+    git checkout $1
   fi &&\
-  git checkout $branch &&\
   (cd plugin-template && zip -9r ../nanome/plugin-template.zip .) &&\
   pip install --upgrade . &&\
   rm -rf ../nanome-lib
