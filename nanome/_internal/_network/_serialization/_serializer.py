34ef3af8494e276c688ab57b55293986328c3a0c
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-04-19 13:40:27 -0400

    Add callback function parameters to PluginInstance Docstrings autopep8 plugin_instance.py
    Add type hinting for 2 callback functions
    
    More callback documentation
    
    Update plugin_instance.py
    
    Update plugin_instance.py
    
    pep8/ undeprecate
    
    Add updated type hints for request_menu_transform
    
    Move callback annotations into docstring to support python 2.7
    
    Update plugin_instance.py
    
    Update plugin_instance.py
    
    PR revisions
    
    Update _serializer.py

--- nanome/_internal/_network/_serialization/_serializer.py
+++ nanome/_internal/_network/_serialization/_serializer.py
@@ -3,10 +3,12 @@ from .._commands import _callbacks as CommandCallbacks
 from .._commands import _serialization as CommandSerializers
 from nanome._internal._util import _serializers as Serializers
 from nanome.util import Logs
-import struct, traceback
+import struct
+import traceback
 
 MESSAGE_VERSION_KEY = "ToClientProtocol"
 packet_debugging = False
+
 
 class Serializer(object):
     _commands = dict()
@@ -18,17 +20,17 @@ class Serializer(object):
         context.write_uint(request_id)
         command_hash = CommandCallbacks._Hashes.MessageHashes[message_type]
         context.write_uint(command_hash)
-        if version_table != None:
+        if version_table is not None:
             if version_table.get(MESSAGE_VERSION_KEY, 0) >= 1:
                 context.write_bool(expects_response)
 
-        if arg != None:
+        if arg is not None:
             command = None
             try:
                 command = Serializer._messages[command_hash]
             except KeyError:
                 Logs.warning("Trying to serialize an unregistered message type:", message_type)
-            if command != None:
+            if command is not None:
                 context.write_using_serializer(command, arg)
                 return context.to_array()
         return context.to_array()
@@ -40,7 +42,7 @@ class Serializer(object):
             command_hash = context.read_uint()
             command = Serializer._commands[command_hash]
         except KeyError:
-            if self.try_register_session(payload) == True:
+            if self.try_register_session(payload) is True:
                 Logs.error("A session is trying to connect even though it is already connected")
             else:
                 Logs.error("Received an unregistered command:", command_hash)
@@ -68,26 +70,28 @@ class Serializer(object):
 
     def try_register_session(self, payload):
         context = _ContextDeserialization(payload, None, packet_debugging)
-        context.read_uint() # Read the request ID
+        context.read_uint()  # Read the request ID
         command_hash = context.read_uint()
         return command_hash == CommandCallbacks._Hashes.CommandHashes[CommandCallbacks._Commands.connect]
 
     def __init__(self):
         self._plugin_id = 0
 
-#-------------Commands-----------#
+# -------------Commands----------- #
 # Commands are incoming (nanome -> plugin)
+
 
 def add_command(command, serializer, callback):
     Serializer._commands[CommandCallbacks._Hashes.CommandHashes[command]] = serializer
     Serializer._command_callbacks[CommandCallbacks._Hashes.CommandHashes[command]] = callback
 
-#control
+
+# control
 add_command(CommandCallbacks._Commands.connect, CommandSerializers._Connect(), CommandCallbacks._connect)
 add_command(CommandCallbacks._Commands.run, CommandSerializers._Run(), CommandCallbacks._run)
 add_command(CommandCallbacks._Commands.advanced_settings, CommandSerializers._AdvancedSettings(), CommandCallbacks._advanced_settings)
 
-#workspace
+# workspace
 add_command(CommandCallbacks._Commands.workspace_response, CommandSerializers._ReceiveWorkspace(), CommandCallbacks._simple_callback_arg)
 add_command(CommandCallbacks._Commands.complex_add, CommandSerializers._ComplexAddedRemoved(), CommandCallbacks._complex_added)
 add_command(CommandCallbacks._Commands.complex_remove, CommandSerializers._ComplexAddedRemoved(), CommandCallbacks._complex_removed)
@@ -102,10 +106,10 @@ add_command(CommandCallbacks._Commands.complex_updated, CommandSerializers._Comp
 add_command(CommandCallbacks._Commands.selection_changed, CommandSerializers._SelectionChanged(), CommandCallbacks._selection_changed)
 add_command(CommandCallbacks._Commands.compute_hbonds_done, CommandSerializers._ComputeHBonds(), CommandCallbacks._simple_callback_no_arg)
 
-#Volume
+# Volume
 add_command(CommandCallbacks._Commands.add_volume_done, CommandSerializers._AddVolumeDone(), CommandCallbacks._simple_callback_no_arg)
 
-#ui
+# ui
 add_command(CommandCallbacks._Commands.menu_toggle, CommandSerializers._MenuCallback(), CommandCallbacks._menu_toggled)
 add_command(CommandCallbacks._Commands.button_press, CommandSerializers._ButtonCallback(), CommandCallbacks._button_pressed)
 add_command(CommandCallbacks._Commands.button_hover, CommandSerializers._ButtonCallback(), CommandCallbacks._button_hover)
@@ -119,13 +123,8 @@ add_command(CommandCallbacks._Commands.image_release, CommandSerializers._ImageC
 add_command(CommandCallbacks._Commands.dropdown_item_click, CommandSerializers._DropdownCallback(), CommandCallbacks._dropdown_item_clicked)
 add_command(CommandCallbacks._Commands.menu_transform_response, CommandSerializers._GetMenuTransformResponse(), CommandCallbacks._simple_callback_arg_unpack)
 
-#files deprecated
-add_command(CommandCallbacks._Commands.directory_response, CommandSerializers._DirectoryRequest(), CommandCallbacks._simple_callback_arg)
-add_command(CommandCallbacks._Commands.file_response, CommandSerializers._FileRequest(), CommandCallbacks._simple_callback_arg)
-add_command(CommandCallbacks._Commands.file_save_done, CommandSerializers._FileSave(), CommandCallbacks._simple_callback_arg)
+# files
 add_command(CommandCallbacks._Commands.export_files_result, CommandSerializers._ExportFiles(), CommandCallbacks._simple_callback_arg)
-
-#files
 add_command(CommandCallbacks._Commands.print_working_directory_response, CommandSerializers._PWD(), CommandCallbacks._simple_callback_arg_unpack)
 add_command(CommandCallbacks._Commands.cd_response, CommandSerializers._CD(), CommandCallbacks._simple_callback_arg)
 add_command(CommandCallbacks._Commands.ls_response, CommandSerializers._LS(), CommandCallbacks._simple_callback_arg_unpack)
@@ -137,13 +136,13 @@ add_command(CommandCallbacks._Commands.rm_response, CommandSerializers._RM(), Co
 add_command(CommandCallbacks._Commands.rmdir_response, CommandSerializers._RMDir(), CommandCallbacks._simple_callback_arg)
 add_command(CommandCallbacks._Commands.mkdir_response, CommandSerializers._MKDir(), CommandCallbacks._simple_callback_arg)
 
-#streams
+# streams
 add_command(CommandCallbacks._Commands.stream_create_done, CommandSerializers._CreateStreamResult(), CommandCallbacks._receive_create_stream_result)
 add_command(CommandCallbacks._Commands.stream_feed, CommandSerializers._FeedStream(), CommandCallbacks._feed_stream)
 add_command(CommandCallbacks._Commands.stream_interrupt, CommandSerializers._InterruptStream(), CommandCallbacks._receive_interrupt_stream)
 add_command(CommandCallbacks._Commands.stream_feed_done, CommandSerializers._FeedStreamDone(), CommandCallbacks._simple_callback_no_arg)
 
-#macros
+# macros
 add_command(CommandCallbacks._Commands.get_macros_response, CommandSerializers._GetMacrosResponse(), CommandCallbacks._simple_callback_arg)
 add_command(CommandCallbacks._Commands.run_macro_result, CommandSerializers._RunMacro(), CommandCallbacks._simple_callback_arg)
 
@@ -156,21 +155,23 @@ add_command(CommandCallbacks._Commands.controller_transforms_response, CommandSe
 add_command(CommandCallbacks._Commands.set_shape_result, CommandSerializers._SetShape(), CommandCallbacks._simple_callback_arg_unpack)
 add_command(CommandCallbacks._Commands.delete_shape_result, CommandSerializers._DeleteShape(), CommandCallbacks._simple_callback_arg)
 
-#others
+# others
 add_command(CommandCallbacks._Commands.load_file_done, CommandSerializers._LoadFileDone(), CommandCallbacks._simple_callback_arg)
 add_command(CommandCallbacks._Commands.integration, CommandSerializers._Integration(), CommandCallbacks._integration)
 
-#-------------Messages-----------#
+# -------------Messages----------- #
 # Messages are outgoing (plugin -> nanome)
+
 
 def add_message(command, serializer):
     Serializer._messages[CommandCallbacks._Hashes.MessageHashes[command]] = serializer
 
+
 Serializers._type_serializer._TypeSerializer.register_string_raw(MESSAGE_VERSION_KEY, 1)
-#control
+# control
 add_message(CommandCallbacks._Messages.connect, CommandSerializers._Connect())
 
-#workspace
+# workspace
 add_message(CommandCallbacks._Messages.workspace_update, CommandSerializers._UpdateWorkspace())
 add_message(CommandCallbacks._Messages.structures_deep_update, CommandSerializers._UpdateStructures(False))
 add_message(CommandCallbacks._Messages.structures_shallow_update, CommandSerializers._UpdateStructures(True))
@@ -186,10 +187,10 @@ add_message(CommandCallbacks._Messages.hook_complex_updated, CommandSerializers.
 add_message(CommandCallbacks._Messages.hook_selection_changed, CommandSerializers._SelectionChangedHook())
 add_message(CommandCallbacks._Messages.compute_hbonds, CommandSerializers._ComputeHBonds())
 
-#volume
+# volume
 add_message(CommandCallbacks._Messages.add_volume, CommandSerializers._AddVolume())
 
-#ui
+# ui
 add_message(CommandCallbacks._Messages.menu_update, CommandSerializers._UpdateMenu())
 add_message(CommandCallbacks._Messages.content_update, CommandSerializers._UpdateContent())
 add_message(CommandCallbacks._Messages.node_update, CommandSerializers._UpdateNode())
@@ -198,14 +199,7 @@ add_message(CommandCallbacks._Messages.menu_transform_request, CommandSerializer
 add_message(CommandCallbacks._Messages.notification_send, CommandSerializers._SendNotification())
 add_message(CommandCallbacks._Messages.hook_ui_callback, CommandSerializers._UIHook())
 
-#files deprecated
-add_message(CommandCallbacks._Messages.directory_request, CommandSerializers._DirectoryRequest())
-add_message(CommandCallbacks._Messages.file_request, CommandSerializers._FileRequest())
-add_message(CommandCallbacks._Messages.file_save, CommandSerializers._FileSave())
-add_message(CommandCallbacks._Messages.export_files, CommandSerializers._ExportFiles())
-add_message(CommandCallbacks._Messages.plugin_list_button_set, CommandSerializers._SetPluginListButton())
-
-#files
+# files
 add_message(CommandCallbacks._Messages.print_working_directory, CommandSerializers._PWD())
 add_message(CommandCallbacks._Messages.cd, CommandSerializers._CD())
 add_message(CommandCallbacks._Messages.ls, CommandSerializers._LS())
@@ -217,14 +211,14 @@ add_message(CommandCallbacks._Messages.rm, CommandSerializers._RM())
 add_message(CommandCallbacks._Messages.rmdir, CommandSerializers._RMDir())
 add_message(CommandCallbacks._Messages.mkdir, CommandSerializers._MKDir())
 
-#macros
+# macros
 add_message(CommandCallbacks._Messages.run_macro, CommandSerializers._RunMacro())
 add_message(CommandCallbacks._Messages.save_macro, CommandSerializers._SaveMacro())
 add_message(CommandCallbacks._Messages.delete_macro, CommandSerializers._DeleteMacro())
 add_message(CommandCallbacks._Messages.get_macros, CommandSerializers._GetMacros())
 add_message(CommandCallbacks._Messages.stop_macro, CommandSerializers._StopMacro())
 
-#streams
+# streams
 add_message(CommandCallbacks._Messages.stream_create, CommandSerializers._CreateStream())
 add_message(CommandCallbacks._Messages.stream_feed, CommandSerializers._FeedStream())
 add_message(CommandCallbacks._Messages.stream_destroy, CommandSerializers._DestroyStream())
@@ -237,9 +231,19 @@ add_message(CommandCallbacks._Messages.controller_transforms_request, CommandSer
 add_message(CommandCallbacks._Messages.set_shape, CommandSerializers._SetShape())
 add_message(CommandCallbacks._Messages.delete_shape, CommandSerializers._DeleteShape())
 
-#others
+# others
 add_message(CommandCallbacks._Messages.open_url, CommandSerializers._OpenURL())
 add_message(CommandCallbacks._Messages.load_file, CommandSerializers._LoadFile())
 add_message(CommandCallbacks._Messages.integration, CommandSerializers._Integration())
 add_message(CommandCallbacks._Messages.set_skybox, CommandSerializers._SetSkybox())
 add_message(CommandCallbacks._Messages.apply_color_scheme, CommandSerializers._ApplyColorScheme())
+
+# files deprecated
+add_message(CommandCallbacks._Messages.directory_request, CommandSerializers._DirectoryRequest())
+add_message(CommandCallbacks._Messages.file_request, CommandSerializers._FileRequest())
+add_message(CommandCallbacks._Messages.file_save, CommandSerializers._FileSave())
+add_message(CommandCallbacks._Messages.export_files, CommandSerializers._ExportFiles())
+add_message(CommandCallbacks._Messages.plugin_list_button_set, CommandSerializers._SetPluginListButton())
+add_command(CommandCallbacks._Commands.directory_response, CommandSerializers._DirectoryRequest(), CommandCallbacks._simple_callback_arg)
+add_command(CommandCallbacks._Commands.file_response, CommandSerializers._FileRequest(), CommandCallbacks._simple_callback_arg)
+add_command(CommandCallbacks._Commands.file_save_done, CommandSerializers._FileSave(), CommandCallbacks._simple_callback_arg)
