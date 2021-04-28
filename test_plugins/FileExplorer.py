66de126afadcaf80ae7794138e12456dc3bf4643
Alex McNerney <ajm13@me.com>
2021-04-28 14:00:44 -0700

    Asyncify FileExplorer test plugin

--- test_plugins/FileExplorer.py
+++ test_plugins/FileExplorer.py
@@ -1,10 +1,10 @@
-import nanome
 import os
 import tempfile
 import ntpath
-from nanome.api.ui import Menu
-from nanome.api.ui import LayoutNode
-from nanome.util.logs import Logs
+
+import nanome
+from nanome.api.ui import Menu, LayoutNode
+from nanome.util import async_callback, Logs
 
 NAME = "File Explorer"
 DESCRIPTION = "Allows you to browse your files"
@@ -14,7 +14,7 @@ HAS_ADVANCED_OPTIONS = False
 test_assets = os.getcwd() + ("/testing/test_assets")
 
 
-class FileExplorer(nanome.PluginInstance):
+class FileExplorer(nanome.AsyncPluginInstance):
 
     def start(self):
         self.running = False
@@ -29,8 +29,7 @@ class FileExplorer(nanome.PluginInstance):
         self.back_button = self.menu.root.find_node("back", True).get_content()
         self.back_button.register_pressed_callback(self.back_pressed)
         self.selected_button = None
-        self.fetch_children()
-        self.fetch_wd()
+        self.fetch_path()
         self.temp_dir = tempfile.mkdtemp()
         # self.test_path = "C:\\Users\\ETHANV~1\\AppData\\Local\\Temp\\tmpuzepx_cf\\file.jpg"
         # self.test_path1 = "C:\\Users\\ETHANV~1\\AppData\\Local\\Temp\\tmpuzepx_cf\\1.jpg"
@@ -40,33 +39,27 @@ class FileExplorer(nanome.PluginInstance):
         # self.files.put(self.test_path, self.test_path2, self.put_done)
         # self.files.mv(self.test_path, self.test_path3, self.mv_done)
 
-    def cp_done(self, *args):
-        Logs.debug("cp done")
+    # def cp_done(self, *args):
+    #     Logs.debug("cp done")
 
-    def put_done(self, *args):
-        Logs.debug("put done")
+    # def put_done(self, *args):
+    #     Logs.debug("put done")
 
-    def mv_done(self, *args):
-        Logs.debug("mv done")
+    # def mv_done(self, *args):
+    #     Logs.debug("mv done")
 
     def on_run(self):
         self.running = True
         self.update_menu(self.menu)
 
-    def fetch_wd(self):
-        self.wd = "."
-        self.files.pwd(self.__set_dir)
-
-    def __set_dir(self, error, wd):
-        self.wd = wd
+    @async_callback
+    async def fetch_path(self, *_):
+        error, wd = await self.files.pwd()
         self.path_text.text_value = wd
         if self.running:
             self.update_content(self.path_text)
 
-    def fetch_children(self):
-        self.files.ls(".", self.__set_children)
-
-    def __set_children(self, error, files):
+        error, files = await self.files.ls(".")
         if error != nanome.util.FileError.no_error: # If API couldn't access directory, display error
             nanome.util.Logs.error("Directory request error:", str(error))
             return
@@ -90,7 +83,7 @@ class FileExplorer(nanome.PluginInstance):
 
     def entry_pressed(self, button):
         if button.entry.is_directory:
-            self.files.cd(button.entry.name, self.directory_changed)
+            self.files.cd(button.entry.name, self.fetch_path)
             return
         to_update = []
         button.selected = True
@@ -109,12 +102,14 @@ class FileExplorer(nanome.PluginInstance):
         to_update.append(self.load_button)
         self.update_content(to_update)
 
-    def load_pressed(self, button):
+    @async_callback
+    async def load_pressed(self, button):
         entry = self.selected_button.entry
-        if not entry.is_directory:
-            self.files.get(entry.name, os.path.join(self.temp_dir, str(self.path_leaf(entry.name))), self.file_fetched)
+        if entry.is_directory:
+            return
 
-    def file_fetched(self, error, path):
+        path = os.path.join(self.temp_dir, str(self.path_leaf(entry.name)))
+        error, _ = await self.files.get(entry.name, path)
         if error == nanome.util.FileError.no_error:
             Logs.debug(path)
             self.send_files_to_load(path)
@@ -125,13 +120,10 @@ class FileExplorer(nanome.PluginInstance):
         pass
 
     def back_pressed(self, button):
-        self.files.cd("..", self.directory_changed)
-
-    def directory_changed(self, *args):
-        self.fetch_wd()
-        self.fetch_children()
+        self.files.cd("..", self.fetch_path)
 
     def path_leaf(self, path):
         head, tail = ntpath.split(path)
         return tail or ntpath.basename(head)
+
 nanome.Plugin.setup(NAME, DESCRIPTION, CATEGORY, HAS_ADVANCED_OPTIONS, FileExplorer)
