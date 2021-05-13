2602fd352a1b5f8542e0ab25f4309b534f014a4f
Max Howard <max@nanome.ai>
2021-05-13 13:55:47 -0700

    presenter info

--- nanome/api/user/presenter_info.py
+++ nanome/api/user/presenter_info.py
@@ -1,4 +1,7 @@
 class PresenterInfo():
+    """
+    | Class to fetch information about the current nanome session's presenter.
+    """
     def __init__(self):
         self._account_id = ""
         self._account_name = ""
@@ -6,6 +9,11 @@ class PresenterInfo():
 
     @property
     def account_id(self):
+        """
+        Get the nanome account ID of the presenter
+
+        :type: string
+        """
         return self._account_id
     @account_id.setter
     def account_id(self, value):
@@ -13,6 +21,11 @@ class PresenterInfo():
 
     @property
     def account_name(self):
+        """
+        Get the nanome account name of the presenter
+        
+        :type: string
+        """
         return self._account_name
     @account_name.setter
     def account_name(self, value):
@@ -20,6 +33,11 @@ class PresenterInfo():
 
     @property
     def account_email(self):
+        """
+        Get the nanome account email of the presenter
+        
+        :type: string
+        """
         return self._account_email
     @account_email.setter
     def account_email(self, value):
