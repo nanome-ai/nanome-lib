3ec9a30066c4979e999b356743429f72c3749523
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-06-17 17:38:59 -0400

    Add Test for new Color properties/ clean up some testing code

---
+++ testing/color_tests.py
@@ -0,0 +1,26 @@
+from nanome.util import Color
+from testing.utilities import run_test
+import unittest
+
+
+def run(counter):
+    testcase = TestColorProperties()
+    testcase.setUp()
+    run_test(testcase.test_rgb, counter)
+    run_test(testcase.test_rgba, counter)
+
+
+class TestColorProperties(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.color = Color.Black()
+
+    def test_rgb(self):
+        rgb = self.color.rgb
+        self.assertTrue(isinstance(rgb, tuple))
+        self.assertEqual(len(rgb), 3)
+
+    def test_rgba(self):
+        rgba = self.color.rgba
+        self.assertTrue(isinstance(rgba, tuple))
+        self.assertEqual(len(rgba), 4)
