87dc472ff432553ddb1bd1af6593904323095bab
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-06-17 18:14:37 -0400

    Make tests compatible with 2.7

--- testing/color_tests.py
+++ testing/color_tests.py
@@ -1,28 +1,23 @@
 from nanome.util import Color
 from testing.utilities import run_test
-import unittest
 
 
 def run(counter):
-    # Manually call all the things TestCases should do automatically, so that it
-    # plays nice with our existing unittests
     testcase = TestColorProperties()
-    testcase.setUp()
     run_test(testcase.test_rgb, counter)
     run_test(testcase.test_rgba, counter)
 
 
-class TestColorProperties(unittest.TestCase):
-    def setUp(self):
-        super().setUp()
-        self.color = Color.Black()
+class TestColorProperties:
 
     def test_rgb(self):
-        rgb = self.color.rgb
-        self.assertTrue(isinstance(rgb, tuple))
-        self.assertEqual(len(rgb), 3)
+        color = Color.Black()
+        rgb = color.rgb
+        assert isinstance(rgb, tuple)
+        assert len(rgb) == 3
 
     def test_rgba(self):
-        rgba = self.color.rgba
-        self.assertTrue(isinstance(rgba, tuple))
-        self.assertEqual(len(rgba), 4)
+        color = Color.Black()
+        rgba = color.rgba
+        assert isinstance(rgba, tuple)
+        assert len(rgba) == 4
