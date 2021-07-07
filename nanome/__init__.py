19d2ba6be3ad39ffd743ac357ba501915c099909
Alex McNerney <ajm13@me.com>
2021-07-07 13:08:48 -0700

    Bump version: 0.27.5 → 0.27.6

--- nanome/__init__.py
+++ nanome/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.27.5"
+__version__ = "0.27.6"
 
 from . import *
 from . import util
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 setup(
     name = 'nanome',
     packages=find_packages(exclude=["testing","doc","test_plugins"]),
-    version = '0.27.5',
+    version = '0.27.6',
     license='MIT',
     description = 'Python API for Nanome Plugins',
     long_description = README,
