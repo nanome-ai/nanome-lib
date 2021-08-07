2a73a6c30cd402d198d9e18610d7ef7d50022151
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2021-08-06 18:31:18 -0700

    added open url in desktop option and made open in vr the default

--- nanome/_internal/_network/_commands/_serialization/_open_url.py
+++ nanome/_internal/_network/_commands/_serialization/_open_url.py
@@ -7,13 +7,15 @@ class _OpenURL(_TypeSerializer):
         self.string = _StringSerializer()
 
     def version(self):
-        return 0
+        return 1
 
     def name(self):
         return "OpenURL"
 
     def serialize(self, version, value, context):
-        context.write_using_serializer(self.string, value)
+        context.write_using_serializer(self.string, value[0]) # URL
+        if version >= 1:
+            context.write_bool(value[1]) # Desktop Browser
 
     def deserialize(self, version, context):
         raise NotImplementedError
