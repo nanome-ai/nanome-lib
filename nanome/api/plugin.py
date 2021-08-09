338827df3487fc62b4734ac16db9eab070dd56ad
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-08-09 13:24:48 -0400

    Argparser refactor (#219)
    *    Move command line arg parsing into Plugin, since they are set by the user.
    *    Use argparse library instead of manually looping through sys.argv
    *    General spacing and formatting fixes.

--- nanome/api/plugin.py
+++ nanome/api/plugin.py
@@ -1,13 +1,11 @@
+import argparse
+
 from . import _DefaultPlugin
-from nanome._internal import _network as Network, _Plugin, _PluginInstance
+from nanome._internal import _Plugin
 from nanome._internal._process import _ProcessManager
-from nanome._internal._network._serialization._serializer import Serializer
 from nanome.util.logs import Logs
 from nanome.util import config
-from multiprocessing import Process, Pipe
-import sys
-import json
-import cProfile
+
 
 class Plugin(_Plugin):
     """
@@ -23,8 +21,24 @@ class Plugin(_Plugin):
     :param has_advanced: If true, plugin will display an "Advanced Settings" button
     :type has_advanced: :class:`bool`
     """
+
+    def create_parser(self):
+        """Command Line Interface for Plugins.
+
+        rtype: argsparser: args parser
+        """
+        parser = argparse.ArgumentParser(description='Starts a Nanome Plugin.')
+        parser.add_argument('-a', '--host', help='connects to NTS at the specified IP address')
+        parser.add_argument('-p', '--port', type=int, help='connects to NTS at the specified port')
+        parser.add_argument('-r', '--auto-reload', action='store_true', help='Restart plugin automatically if a .py or .json file in current directory changes')
+        parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose mode, to display Logs.debug')
+        parser.add_argument('-n', '--name', help='Name to display for this plugin in Nanome', default='')
+        parser.add_argument('-k', '--keyfile', default='', help='Specifies a key file or key string to use to connect to NTS')
+        parser.add_argument('-i', '--ignore', help='To use with auto-reload. All paths matching this pattern will be ignored, use commas to specify several. Supports */?/[seq]/[!seq]', default='')
+        return parser
+
     @classmethod
-    def setup(cls, name, description, tags, has_advanced, plugin_class, host = "config", port = "config", key = "config", permissions=[], integrations=[]):
+    def setup(cls, name, description, tags, has_advanced, plugin_class, host="config", port="config", key="config", permissions=[], integrations=[]):
         if not _Plugin._is_process():
             plugin = cls(name, description, tags, has_advanced, permissions, integrations)
             plugin.set_plugin_class(plugin_class)
@@ -44,7 +58,7 @@ class Plugin(_Plugin):
     def set_maximum_processes_count(max_process_nb):
         _ProcessManager._max_process_count = max_process_nb
 
-    def run(self, host = "config", port = "config", key = "config"):
+    def run(self, host="config", port="config", key="config"):
         """
         | Starts the plugin by connecting to the server specified.
         | If arguments (-a, -p) are given when starting plugin, host/port will be ignored.
@@ -55,19 +69,29 @@ class Plugin(_Plugin):
         :type host: str
         :type port: int
         """
-        if (host == "config"):
-            self.__host = config.fetch("host")
-        else:
-            self.__host = host
-        if (port == "config"):
-            self.__port = config.fetch("port")
-        else:
-            self.__port = port
-        if (key == "config"):
-            self.__key = config.fetch("key")
-        else:
-            self.__key = key
-        self.__parse_args()
+        default_host = config.fetch('host') if host == 'config' else host
+        default_port = config.fetch('port') if port == 'config' else port
+        default_key = config.fetch('key') if key == 'config' else key
+
+        # Parse command line args and set internal variables.
+        parser = self.create_parser()
+        args = parser.parse_args()
+
+        self.__host = args.host or default_host
+        self.__port = args.port or default_port
+        self.__key = args.keyfile or default_key
+        self.__has_autoreload = args.auto_reload
+        self.__has_verbose = args.verbose
+        Logs._set_verbose(args.verbose)
+
+        if args.ignore:
+            to_ignore = args.ignore.split(",")
+            self.__to_ignore.extend(to_ignore)
+
+        # Name can be set during the class instantiation without cli arg.
+        if args.name:
+            self._description['name'] = args.name
+
         Logs.debug("Start plugin")
         if self.__has_autoreload:
             self.__autoreload()
@@ -91,6 +115,7 @@ class Plugin(_Plugin):
         | Useful when using autoreload
         """
         return self._pre_run
+
     @pre_run.setter
     def pre_run(self, value):
         self._pre_run = value
@@ -102,10 +127,11 @@ class Plugin(_Plugin):
         | Useful when using autoreload
         """
         return self._post_run
+
     @post_run.setter
     def post_run(self, value):
         self._post_run = value
 
-    def __init__(self, name, description, tags=[], has_advanced = False, permissions=[], integrations=[]):
+    def __init__(self, name, description, tags=[], has_advanced=False, permissions=[], integrations=[]):
         super(Plugin, self).__init__(name, description, tags, has_advanced, permissions, integrations)
         self._plugin_class = _DefaultPlugin
