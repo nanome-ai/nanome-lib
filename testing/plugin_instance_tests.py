27b2d9e98b8fb11739654d99c8b33915208d1ff8
mjrosengrant <mike@rosengrant.me>
2021-11-03 12:09:46 -0400

    100% code coverage on plugin_instance.py

--- testing/plugin_instance_tests.py
+++ testing/plugin_instance_tests.py
@@ -1,9 +1,12 @@
+import os
 import sys
 import unittest
+import uuid
 
 from nanome import PluginInstance
+from nanome.api.plugin_instance import _DefaultPlugin
 from nanome.api import structure, ui
-from nanome.util import enums
+from nanome.util import enums, Vector3, Quaternion, config
 
 if sys.version_info.major >= 3:
     from unittest.mock import MagicMock
@@ -15,6 +18,7 @@ else:
 class PluginInstanceTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.custom_data = {'a': 'b'}
         self.plugin_instance = PluginInstance()
 
         # Attributes normally set by _PluginInstance Network Instantiation
@@ -22,10 +26,9 @@ class PluginInstanceTestCase(unittest.TestCase):
         self.plugin_instance._run_usable = True
         self.plugin_instance._advanced_settings_text = "Advanced Settings"
         self.plugin_instance._advanced_settings_usable = True
-        self.plugin_instance._custom_data = {}
+        self.plugin_instance._custom_data = self.custom_data
         self.plugin_instance._permissions = []
         self.plugin_instance._menus = {}
-
         self.plugin_instance._network = MagicMock()
 
     def test_on_advanced_settings(self):
@@ -99,12 +102,17 @@ class PluginInstanceTestCase(unittest.TestCase):
         self.plugin_instance.create_writing_stream(index_list, stream_type)
 
     def test_open_url(self):
-        url = 'https://nanome.ai'
+        url = 'nanome.ai'
         self.plugin_instance.open_url(url, desktop_browser=True)
         self.plugin_instance.open_url(url, desktop_browser=False)
 
     def test_plugin_files_path(self):
+        # Change config to be a file that doesn't exist, so we test creating new directory.
+        starting_file_path = config.fetch('plugin_files_path')
+        new_file_path = '/tmp/' + str(uuid.uuid4())
+        config.set('plugin_files_path', new_file_path)
         self.plugin_instance.plugin_files_path
+        config.set('plugin_files_path', starting_file_path)
 
     def test_request_complex_list(self):
         self.plugin_instance.request_complex_list()
@@ -121,7 +129,8 @@ class PluginInstanceTestCase(unittest.TestCase):
         self.plugin_instance.request_directory(path)
 
     def test_request_export(self):
-        self.plugin_instance.request_export(enums.ExportFormats.SDF)
+        entity = structure.Complex()
+        self.plugin_instance.request_export(enums.ExportFormats.SDF, entities=entity)
 
     def test_request_files(self):
         self.plugin_instance.request_files([])
@@ -132,6 +141,11 @@ class PluginInstanceTestCase(unittest.TestCase):
     def test_request_presenter_info(self):
         self.plugin_instance.request_presenter_info()
 
+    def test_menu(self):
+        self.plugin_instance.menu
+        self.plugin_instance.menu = ui.Menu()
+        self.plugin_instance.menu
+
     def test_request_workspace(self):
         self.plugin_instance.request_workspace()
 
@@ -139,7 +153,14 @@ class PluginInstanceTestCase(unittest.TestCase):
         self.plugin_instance.save_files([])
 
     def test_send_files_to_load(self):
-        self.plugin_instance.send_files_to_load([])
+        test_assets = os.getcwd() + ("/testing/test_assets")
+        test_path = test_assets + "/test_menu.json"
+        # Test different input formats supported by function
+        file_list = [test_path]
+        test_tuple = (test_path, 'test_menu.json')
+        self.plugin_instance.send_files_to_load(file_list)
+        self.plugin_instance.send_files_to_load(test_path)
+        self.plugin_instance.send_files_to_load(test_tuple)
 
     def test_send_notification(self):
         notif_type = enums.NotificationTypes.success
@@ -147,15 +168,18 @@ class PluginInstanceTestCase(unittest.TestCase):
         self.plugin_instance.send_notification(notif_type, msg)
 
     def test_set_menu_transform(self):
-        index = None
-        position = None
-        rotation = None
-        scale = None
+        index = 1
+        position = Vector3(0, 0, 0)
+        rotation = Quaternion(1, 1, 1, 1)
+        scale = Vector3(1, 2, 3)
         self.plugin_instance.set_menu_transform(index, position, rotation, scale)
 
     def test_set_plugin_list_button(self):
-        btn = ui.Button()
-        self.plugin_instance.set_plugin_list_button(btn)
+        btn_type = enums.PluginListButtonType.run
+        self.plugin_instance.set_plugin_list_button(btn_type)
+        advanced_settings_btn_type = enums.PluginListButtonType.advanced_settings
+        self.plugin_instance.set_plugin_list_button(
+            advanced_settings_btn_type, text='test_text', usable=True)
 
     def test_start(self):
         self.plugin_instance.start()
@@ -166,6 +190,7 @@ class PluginInstanceTestCase(unittest.TestCase):
     def test_update_content(self):
         btn = ui.Button()
         self.plugin_instance.update_content(btn)
+        self.plugin_instance.update_content([btn])
 
     def test_update_menu(self):
         menu = ui.Menu()
@@ -174,6 +199,7 @@ class PluginInstanceTestCase(unittest.TestCase):
     def test_update_node(self):
         node = ui.LayoutNode()
         self.plugin_instance.update_node(node)
+        self.plugin_instance.update_node([node])
 
     def test_update_structures_deep(self):
         comp = structure.Complex()
@@ -190,3 +216,11 @@ class PluginInstanceTestCase(unittest.TestCase):
     def test_zoom_on_structures(self):
         comp = structure.Complex()
         self.plugin_instance.zoom_on_structures([comp])
+
+    def test_default_plugin(self):
+        # Make sure we can instantiate _DefaultPlugin
+        default_plugin = _DefaultPlugin()
+        self.assertTrue(isinstance(default_plugin, PluginInstance))
+
+    def test_custom_data(self):
+        self.assertEqual(self.plugin_instance.custom_data, self.custom_data)
