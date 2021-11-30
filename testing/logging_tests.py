8962ad6f9baccd7936cdc8a4095639906b1938d2
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-11-30 18:21:30 -0500

    Update logging_tests.py

--- testing/logging_tests.py
+++ testing/logging_tests.py
@@ -20,35 +20,6 @@ class LoggingTestCase(unittest.TestCase):
 
     @patch('nanome._internal._plugin._Plugin._loop')
     @patch('nanome._internal._plugin.Network._NetInstance')
-    def test_log_manager_creation(self, netinstance_mock, loop_mock):
-        host = 'anyhost'
-        port = 8000
-        key = ''
-
-        # Set write_log_file to False, and verify LogsManager is still created.
-        write_log_file = 0
-        testargs = [
-            'run.py',
-            '--write-log-file', write_log_file,
-        ]
-        with patch.object(sys, 'argv', testargs):
-            self.plugin.run(host, port, key)
-        self.assertEqual(self.plugin.write_log_file, False)
-        self.assertTrue(isinstance(self.plugin._logs_manager, _LogsManager))
-
-        # Set write_log_file to True, and verify LogsManager is still created.
-        write_log_file = "True"
-        testargs = [
-            'run.py',
-            '--write-log-file', write_log_file,
-        ]
-        with patch.object(sys, 'argv', testargs):
-            self.plugin.run(host, port, key)
-        self.assertEqual(self.plugin.write_log_file, True)
-        self.assertTrue(isinstance(self.plugin._logs_manager, _LogsManager))
-
-    @patch('nanome._internal._plugin._Plugin._loop')
-    @patch('nanome._internal._plugin.Network._NetInstance')
     @patch('nanome._internal._process._logs_manager.NTSLoggingHandler.handle')
     def test_nts_handler_called(self, handle_mock, netinstance_mock, loop_mock):
         """Assert logs get forwarded to NTS."""
