81cbd8604cf481c815d6356a55e531a2999d4007
Gitbot <gitbot@nanome.ai>
2021-12-09 16:56:09 +0000

    Automated autopep8 fixes

--- nanome/settings.py
+++ nanome/settings.py
@@ -1,14 +1,16 @@
 LOGGING:
-  handlers:
-    console:
-      class : logging.StreamHandler
-      formatter: brief
-      level   : INFO
-      filters: [allow_foo]
-      stream  : ext://sys.stdout
-    file:
-      class : logging.handlers.RotatingFileHandler
-      formatter: precise
-      filename: logconfig.log
-      maxBytes: 1024
-      backupCount: 3
+    handlers:
+        console:
+            class:
+                logging.StreamHandler
+            formatter: brief
+            level: INFO
+            filters: [allow_foo]
+            stream: ext: // sys.stdout
+        file:
+            class:
+                logging.handlers.RotatingFileHandler
+            formatter: precise
+            filename: logconfig.log
+            maxBytes: 1024
+            backupCount: 3
