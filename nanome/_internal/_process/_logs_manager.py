6246042fcae70e4a4e76811feebb95d4214ad6c1
Ramjii <jeremie@nanome.ai>
2021-12-21 17:36:54 +0100

    Fix colors

--- nanome/_internal/_process/_logs_manager.py
+++ nanome/_internal/_process/_logs_manager.py
@@ -75,12 +75,11 @@ class ColorFormatter(logging.Formatter):
     https://stackoverflow.com/a/56944256
     """
 
-    grey = "\x1b[38;21m"
-    yellow = "\x1b[33;21m"
-    red = "\x1b[31;21m"
-    bold_red = "\x1b[31;1m"
+    grey = "\x1b[0m"
+    yellow = "\x1b[33m"
+    red = "\x1b[91m"
+    bold_red = "\x1b[91m"
     reset = "\x1b[0m"
-    no_underline = "\x1b[24m"
     formats = {}
 
     def __init__(self, fmt=None, **kwargs):
@@ -88,8 +87,8 @@ class ColorFormatter(logging.Formatter):
         self.formats = {
             logging.DEBUG: self.grey + fmt + self.reset,
             logging.INFO: self.grey + fmt + self.reset,
-            logging.WARNING: self.yellow + self.no_underline + fmt + self.reset,
-            logging.ERROR: self.red + self.no_underline + fmt + self.reset,
+            logging.WARNING: self.yellow + fmt + self.reset,
+            logging.ERROR: self.red + fmt + self.reset,
             logging.CRITICAL: self.bold_red + fmt + self.reset
         }
 
@@ -104,41 +103,7 @@ class ColorFormatter(logging.Formatter):
 
     @staticmethod
     def supports_color():
-        """Return True if the running system's terminal supports color, False otherwise.
-
-        source: https://github.com/django/django/blob/main/django/core/management/color.py#L20
-        """
-        def vt_codes_enabled_in_windows_registry():
-            """
-            Check the Windows Registry to see if VT code handling has been enabled
-            by default, see https://superuser.com/a/1300251/447564.
-            """
-            try:
-                # winreg is only available on Windows.
-                import winreg
-            except ImportError:
-                return False
-            else:
-                reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Console')
-                try:
-                    reg_key_value, _ = winreg.QueryValueEx(reg_key, 'VirtualTerminalLevel')
-                except FileNotFoundError:
-                    return False
-                else:
-                    return reg_key_value == 1
-
-        # isatty is not always implemented, #6223.
-        is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
-
-        return is_a_tty and (
-            sys.platform != 'win32' or
-            'ANSICON' in os.environ or
-            # Windows Terminal supports VT codes.
-            'WT_SESSION' in os.environ or
-            # Microsoft Visual Studio Code's built-in terminal supports colors.
-            os.environ.get('TERM_PROGRAM') == 'vscode' or
-            vt_codes_enabled_in_windows_registry()
-        )
+        return not sys.platform == 'win32' or not sys.stdout.isatty()
 
 
 class _LogsManager():
