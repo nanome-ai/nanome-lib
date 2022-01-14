553539d09277a7f9c8d6339a7b948d761229ae35
mjrosengrant <mike@rosengrant.me>
2022-01-14 17:22:57 -0500

    remove no-qas

--- nanome/_internal/__init__.py
+++ nanome/_internal/__init__.py
@@ -1,5 +1,5 @@
 from . import *  # noqa F401
-# folders  # noqa F401
+# folders
 from . import _util  # noqa F401
 from . import _shapes  # noqa F401
 from . import _structure  # noqa F401
@@ -7,7 +7,7 @@ from . import _ui  # noqa F401
 from . import _network  # noqa F401
 from . import _volumetric  # noqa F401
 from . import _macro  # noqa F401
-# classes  # noqa F401
+# classes
 from ._addon import _Addon  # noqa F401
 from ._plugin_instance import _PluginInstance  # noqa F401
 from . import _plugin_instance_deprecated  # noqa F401
