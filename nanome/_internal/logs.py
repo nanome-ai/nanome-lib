c71fc183ed74cb3f21863f2fdc324f71d850917e
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-02-02 16:39:49 -0500

    Create NanomeGelfHandler

--- nanome/_internal/logs.py
+++ nanome/_internal/logs.py
@@ -44,56 +44,30 @@ class PipeHandler(logging.Handler):
             pass
 
 
-class NTSFormatter(logging.Formatter):
-    """Send NTS json data with specified log level numbers."""
+class NanomeGelfHandler(graypy.handler.BaseGELFHandler):
+    """Create a GELF dictionary from a log record, including nanome-specific fields."""
 
-    datefmt = "%Y-%m-%dT%H:%M:%S"
-
-    fmt = {
-        'timestamp': '%(asctime)s',
-        'msg': '%(message)s',
-        'sev': '%(levelname)s',  # Will be manually updated to val from LogType enum.
-    }
-
-    def __init__(self, fmt=None, **kwargs):
-        # Use format saved by class, so no need to pass fmt kwarg
-        fmt = json.dumps(self.fmt)
-        super(NTSFormatter, self).__init__(fmt=fmt, datefmt=self.datefmt, **kwargs)
-
-    def format(self, record):
-        return super(NTSFormatter, self).format(record)
-        # msg = super(NTSFormatter, self).format(record)
-        try:
-            json_msg = json.loads(msg.replace('\n', '\\n'))
-        except json.JSONDecodeError:
-            logger.warning('JSON Decode Error in NTSFormatter')
-            updated_msg = msg
-        else:
-            # Convert timestamp to UTC
-            timestamp = json_msg['timestamp']
-            timestamp_dt = parser.parse(timestamp)
-            json_msg['timestamp'] = timestamp_dt.strftime(self.datefmt)
-
-            # Replace `sev` value with corresponding LogType from enum.
-            level_name = json_msg['sev']
-            enum_val = getattr(LogTypes, level_name)
-            json_msg['sev'] = enum_val
-            updated_msg = json.dumps(json_msg)
-        return updated_msg
+    def make_gelf_dict(self, record):
+        gelf_dict = self._make_gelf_dict(record)
+        gelf_dict.update({
+            'plugin_name': self._plugin.name,
+            'plugin_class': self._plugin.plugin_class.__name__,
+            'plugin_id': self._plugin._plugin_id
+        })
+        return gelf_dict
 
 
-class NTSLoggingHandler(graypy.handler.BaseGELFHandler):
+class NTSLoggingHandler(NanomeGelfHandler):
     """Forward Log messages to NTS."""
 
     def __init__(self, plugin):
         super(NTSLoggingHandler, self).__init__()
         self._plugin = plugin
-        self.formatter = NTSFormatter()
 
     def emit(self, record):
         # Use new NTS message format to forward logs
         # fmted_msg = self.formatter.format(record)
-        gelf_dict = self._make_gelf_dict(record)
+        gelf_dict = self.make_gelf_dict(record)
         packet = _Packet()
         packet.set(0, _Packet.packet_type_live_logs, 0)
         packet.write_string(json.dumps(gelf_dict))
