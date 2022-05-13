3079f05c69a33700dc0976454330f14dcffec62d
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-05-13 17:17:04 -0400

    Split out util.Log tests into their own testcase (#308)
    * Write tests for util.Logs that actually validate output
    
    *  use __name__ instead of hardcoding "logging_tests", which makes tests more robust

--- testing/logging_tests.py
+++ testing/logging_tests.py
@@ -12,7 +12,7 @@ else:
     from mock import MagicMock, patch
 
 
-class LoggingTestCase(unittest.TestCase):
+class PluginLoggingTestCase(unittest.TestCase):
 
     def setUp(self):
         self.plugin = Plugin('Test Plugin', 'Unit Test Plugin')
@@ -23,26 +23,29 @@ class LoggingTestCase(unittest.TestCase):
         self.key = ''
 
         # Make it so that Logs logged in this module are handled the same as "nanome"
-        testing_logger = logging.getLogger('logging_tests')
+        testing_logger = logging.getLogger(__name__)
         nanome_logger = logging.getLogger('nanome')
         testing_logger.handlers = nanome_logger.handlers
-        testing_logger.setLevel(logging.DEBUG)
+        testing_logger.setLevel(logging.INFO)
+        nanome_logger.setLevel(logging.WARNING)
+
+        # Hides noisy "Starting Plugin" output
+        logging.getLogger('nanome.api.plugin.Plugin.run').setLevel(logging.ERROR)
 
     @classmethod
     def tearDownClass(cls):
         # Make sure remote logging always off after test.
         # Without this teardown, logging configs persist to tests run after this.
-        super(LoggingTestCase, cls).tearDownClass()
+        super(PluginLoggingTestCase, cls).tearDownClass()
         nanome_logger = logging.getLogger("nanome")
-        testing_logger = logging.getLogger('logging_tests')
-
+        testing_logger = logging.getLogger(__name__)
         nanome_logger.handlers = []
         testing_logger.handlers = []
 
-    @patch('nanome._internal._plugin._Plugin._loop')
+    @patch('nanome._internal._plugin._Plugin._run')
     @patch('nanome._internal._plugin.Network._NetInstance')
     @patch('nanome._internal.logs.NTSLoggingHandler.handle')
-    def test_nts_handler_called(self, handle_mock, netinstance_mock, loop_mock):
+    def test_nts_handler_called(self, handle_mock, *args):
         """Assert logs get forwarded to NTS."""
         remote_logging = "True"
         testargs = [
@@ -57,10 +60,10 @@ class LoggingTestCase(unittest.TestCase):
         Logs.message('This should be forwarded to NTS.')
         handle_mock.assert_called()
 
-    @patch('nanome._internal._plugin._Plugin._loop')
+    @patch('nanome._internal._plugin._Plugin._run')
     @patch('nanome._internal._plugin.Network._NetInstance')
     @patch('nanome._internal.logs.NTSLoggingHandler.handle')
-    def test_nts_handler_not_called(self, handle_mock, netinstance_mock, loop_mock):
+    def test_nts_handler_not_called(self, *args):
         """Assert logs don't get forwarded to NTS if remote-logging is False."""
         remote_logging = False
 
@@ -78,9 +81,9 @@ class LoggingTestCase(unittest.TestCase):
         nts_handler = self.plugin._logs_manager.nts_handler
         self.assertTrue(isinstance(nts_handler, logging.NullHandler))
 
-    @patch('nanome._internal._plugin._Plugin._loop')
+    @patch('nanome._internal._plugin._Plugin._run')
     @patch('nanome._internal._plugin.Network._NetInstance')
-    def test_file_handler_called(self, netinstance_mock, loop_mock):
+    def test_file_handler_called(self, *args):
         """Assert if write_log_file is True, the log_file_handler is utilized."""
         write_log_file = "True"
         testargs = [
@@ -95,9 +98,9 @@ class LoggingTestCase(unittest.TestCase):
         Logs.message('Log file handler should be called.')
         self.plugin._logs_manager.log_file_handler.handle.assert_called()
 
-    @patch('nanome._internal._plugin._Plugin._loop')
+    @patch('nanome._internal._plugin._Plugin._run')
     @patch('nanome._internal._plugin.Network._NetInstance')
-    def test_file_handler_not_called(self, netinstance_mock, loop_mock):
+    def test_file_handler_not_called(self, *args):
         """Assert if write_log_file is False, the log_file_handler is not utilized."""
         write_log_file = False
         testargs = [
@@ -113,27 +116,15 @@ class LoggingTestCase(unittest.TestCase):
     @patch('nanome._internal._plugin._Plugin._loop')
     @patch('nanome._internal._plugin.Network._NetInstance.connect')
     @patch('nanome._internal._plugin.Network._NetInstance.send')
-    def test_nts_logger_handler(self, send_mock, connect_mock, loop_mock):
+    def test_nts_logger_handler(self, send_mock, *args):
         """Ensure NTSLoggingHandler.handle() triggers a network request."""
         with patch.object(sys, 'argv', ['run.py', '--remote-logging', 'True']):
             self.plugin.run(self.host, self.port, self.key)
         send_mock.assert_called()
 
-    @patch('nanome._internal._plugin._Plugin._loop')
-    @patch('nanome._internal._plugin.Network._NetInstance.connect')
-    @patch('nanome._internal._plugin.Network._NetInstance.send')
-    def test_log_types(self, send_mock, connect_mock, loop_mock):
-        with patch.object(sys, 'argv', ['run.py', '-v', '--remote-logging', 'True', '--write-log-file', 'True']):
-            self.plugin.run(self.host, self.port, self.key)
-
-        Logs.warning("This is a warning")
-        Logs.error("This is an error")
-        Logs.debug("This is a debug message")
-        Logs.message("This is a regular message")
-
-    @patch('nanome._internal._plugin._Plugin._loop')
+    @patch('nanome._internal._plugin._Plugin._run')
     @patch('nanome._internal._plugin.Network._NetInstance')
-    def test_console_handler_called(self, netinstance_mock, loop_mock):
+    def test_console_handler_called(self, *args):
         """Assert logs are always logged to the console."""
         testargs = [
             'run.py',
@@ -147,3 +138,38 @@ class LoggingTestCase(unittest.TestCase):
             with patch.object(console_handler, 'handle') as handle_mock:
                 Logs.message("Should be printed to console")
                 handle_mock.assert_called()
+
+
+class LogUtilTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+
+    def test_log_warning(self):
+        with self.assertLogs(self.logger, logging.WARNING) as captured:
+            message = "This is a warning"
+            Logs.warning(message)
+            self.assertEqual(len(captured.records), 1)
+            self.assertEqual(captured.records[0].getMessage(), message)
+
+    def test_log_error(self):
+        with self.assertLogs(self.logger, logging.ERROR) as captured:
+            message = "This is an error"
+            Logs.error(message)
+            self.assertEqual(len(captured.records), 1)
+            self.assertEqual(captured.records[0].getMessage(), message)
+
+    def test_log_debug(self):
+        with self.assertLogs(self.logger, logging.DEBUG) as captured:
+            message = "This is a debug message"
+            Logs.debug(message)
+            self.assertEqual(len(captured.records), 1)
+            self.assertEqual(captured.records[0].getMessage(), message)
+
+    def test_log_message(self):
+        with self.assertLogs(self.logger, logging.INFO) as captured:
+            message = "This is a regular messge"
+            Logs.message(message)
+            self.assertEqual(len(captured.records), 1)
+            self.assertEqual(captured.records[0].getMessage(), message)
