0f7cffe0b76cb0aedc7307e4acdb5eadb7d4ecb0
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-05-25 18:12:09 -0400

    Set up tests for Schemas

---
+++ testing/schema_tests.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+import json
+
+from nanome.api import structure
+from nanome._internal._structure._io._pdb.structure import structure
+
+# Schemas requirements are optional, so don't run tests if they are not installed.
+reqs_installed = False
+try:
+    from nanome.api import schemas
+except ModuleNotFoundError:
+    pass
+else:
+    reqs_installed = True
+
+test_assets = os.path.join(os.getcwd(), "testing/test_assets")
+workspace_json = os.path.join(test_assets, "serialized_data/benzene_workspace.json")
+pdb_file = os.path.join(test_assets, "pdb/1tyl.pdb")
+
+
+@unittest.skipIf(not reqs_installed, "Marshmallow not installed")
+class SerializerTestCase(unittest.TestCase):
+
+    def test_load_workspace(self):
+        with open(workspace_json, 'r') as f:
+            """Deserialize a workspace from JSON."""
+            workspace_data = json.load(f)
+            workspace = schemas.WorkspaceSchema().load(workspace_data)
+            self.assertTrue(isinstance(workspace, structure.Workspace))
+
+    def test_dump_complex(self):
+        # Serialize a complex into JSON.
+        comp = structure.Complex.io.from_pdb(path=pdb_file)
+        self.assertTrue(isinstance(comp, structure.Complex))
+        comp_json = schemas.ComplexSchema().dump(comp)
+        self.assertTrue(isinstance(comp_json, dict))
+        
