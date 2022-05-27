d9797e0b697ade137e0ef952cd7d6c734706455d
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-05-27 17:16:45 -0400

    Rename files to match unittest convention

--- tests.py
+++ tests.py
@@ -3,9 +3,8 @@ import unittest
 import sys
 
 test_directory = 'testing/'
-file_pattern = '*_tests.py'
 
-suite = unittest.TestLoader().discover(test_directory, pattern=file_pattern)
+suite = unittest.TestLoader().discover(test_directory)
 runner = unittest.TextTestRunner(verbosity=1).run(suite)
 
 exit_code = 0 if runner.wasSuccessful() else 1
