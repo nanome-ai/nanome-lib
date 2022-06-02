bc07a82b5f2fff7ffc11c158ae8a436cd21ee3b6
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-06-02 13:59:54 -0400

    remove unusable field from Dropdown

--- nanome/api/schemas/ui_schemas.py
+++ nanome/api/schemas/ui_schemas.py
@@ -465,7 +465,6 @@ class DropdownSchema(Schema):
     use_permanent_title = fields.Bool()
     max_displayed_items = fields.Integer(min=0)
     items = fields.List(fields.Nested(DropdownItemSchema))
-    unusable = fields.Bool(default=False)
 
     @post_load
     def make_obj(self, data, **kwargs):
