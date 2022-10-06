303adcccec93239e6b360fa95bbd50f7b01fe7b4
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-10-06 12:27:09 -0400

    add 12 hour timeout for unclosed Sessions

--- nanome/_internal/_plugin_instance_async.py
+++ nanome/_internal/_plugin_instance_async.py
@@ -6,21 +6,37 @@ from timeit import default_timer as timer
 from nanome.util import Logs
 from nanome.util.asyncio import handle_exception
 
+# End session after 12 hours
+# This should be long enough to indicate
+# a runaway session that wasn't closed by NTS
+SESSION_TIMEOUT = 60 * 60 * 12 
+
 
 async def async_update_loop(plugin_instance, UPDATE_RATE, MINIMUM_SLEEP):
     try:
         plugin_instance.start()
+        loop_start_time = timer()
         last_update = timer()
         while plugin_instance._network._receive() and plugin_instance._process_manager.update():
             plugin_instance.update()
-
             dt = last_update - timer()
             sleep_time = max(UPDATE_RATE - dt, MINIMUM_SLEEP)
             await asyncio.sleep(sleep_time)
             last_update = timer()
+            if last_update - loop_start_time > SESSION_TIMEOUT:
+                Logs.warning("Session timed out")
+                raise TimeoutError()
 
     except KeyboardInterrupt:
         plugin_instance._on_stop()
+        return
+    except TimeoutError:
+        Logs.warning("Session timed out")
+        plugin_name = plugin_instance.__class__.__name__
+        plugin_instance.send_notification(3, f"{plugin_name} Session Timed out")
+        plugin_instance._on_stop()
+        plugin_instance._process_manager._close()
+        plugin_instance._network._close()
         return
     except Exception:
         msg = traceback.format_exc()
