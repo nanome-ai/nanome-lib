509708b42eb0dd0c21e099e2e1fc842bf4e2dea5
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-10-07 10:06:37 -0400

    Use AsyncPluginInstance in plugin template (#326)

--- plugin-template/plugin/Plugin.py
+++ plugin-template/plugin/Plugin.py
@@ -1,9 +1,10 @@
 import nanome
 from nanome.api.ui import Menu
-from nanome.util import Logs
+from nanome.util import async_callback, Logs
 
 
-class {{class}}(nanome.PluginInstance):
+class {{class}}(nanome.AsyncPluginInstance):
+
     def start(self):
         self.menu = Menu()
         self.menu.title = '{{name}}'
@@ -12,10 +13,17 @@ class {{class}}(nanome.PluginInstance):
 
         msg = 'Hello Nanome!'
         node = self.menu.root.create_child_node()
-        node.add_new_label(msg)
+        self.label = node.add_new_label(msg)
         Logs.message(msg)
 
-    def on_run(self):
+    @async_callback
+    async def on_run(self):
+        # Print the number of complexes in the workspace
+        # to the Menu.
+        comps = await self.request_complex_list()
+        msg = f'{len(comps)} Complex(es) in Workspace'
+        Logs.message(msg)
+        self.label.text_value = msg
         self.menu.enabled = True
         self.update_menu(self.menu)
 
