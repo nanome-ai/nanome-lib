f931ffd3679ce81a949976b80f5ce27cdffa7f9b
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-01-02 00:30:23 -0500

    reorder functions in Session class

--- nanome/_internal/network/session.py
+++ nanome/_internal/network/session.py
@@ -8,6 +8,35 @@ stop_bytes = bytearray("CLOSEPIPE", "utf-8")
 
 # Plugin networking representation, used from the main process
 class Session(object):
+
+    def __init__(self, session_id, net_plugin, process_manager, logs_manager, net_queue_out, net_queue_in, pm_queue_out, pm_queue_in):
+        self._session_id = session_id
+        self._net_plugin = net_plugin
+        self._process_manager = process_manager
+        self._logs_manager = logs_manager
+        self._net_queue_out = net_queue_out
+        self._net_queue_in = net_queue_in
+        self._pm_queue_out = pm_queue_out
+        self._pm_queue_in = pm_queue_in
+        self.plugin_process = None
+        self._closed = False
+
+    def send_process_data(self, data):
+        try:
+            self._pm_queue_out.put(data)
+        except Exception:
+            Logs.error("Cannot deliver process info to plugin", self._session_id, "Did it crash?")
+        
+    def signal_and_close_pipes(self):
+        self._on_packet_received(stop_bytes)
+        self._closed = True
+        self.close_pipes()
+
+    def close_pipes(self):
+        self._net_queue_out.close()
+        self._pm_queue_out.close()
+        self._process_manager._remove_session_processes(self._session_id)
+
     def _read_from_plugin(self):
         try:
             has_net_data = not self._net_queue_in.empty()
@@ -37,35 +66,8 @@ class Session(object):
         except Exception:
             Logs.error("Cannot deliver packet to plugin", self._session_id, "Did it crash?")
 
-    def send_process_data(self, data):
-        try:
-            self._pm_queue_out.put(data)
-        except Exception:
-            Logs.error("Cannot deliver process info to plugin", self._session_id, "Did it crash?")
-
     def _send_disconnection_message(self, plugin_id):
         packet = Packet()
         packet.set(self._session_id, Packet.packet_type_plugin_disconnection, plugin_id)
         self._net_plugin.send(packet)
 
-    def signal_and_close_pipes(self):
-        self._on_packet_received(stop_bytes)
-        self._closed = True
-        self.close_pipes()
-
-    def close_pipes(self):
-        self._net_queue_out.close()
-        self._pm_queue_out.close()
-        self._process_manager._remove_session_processes(self._session_id)
-
-    def __init__(self, session_id, net_plugin, process_manager, logs_manager, net_queue_out, net_queue_in, pm_queue_out, pm_queue_in):
-        self._session_id = session_id
-        self._net_plugin = net_plugin
-        self._process_manager = process_manager
-        self._logs_manager = logs_manager
-        self._net_queue_out = net_queue_out
-        self._net_queue_in = net_queue_in
-        self._pm_queue_out = pm_queue_out
-        self._pm_queue_in = pm_queue_in
-        self.plugin_process = None
-        self._closed = False
