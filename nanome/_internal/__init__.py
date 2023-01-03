e6ed4dcd10684e76d99e2f8ca23353bf1977b4fe
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-01-02 23:22:34 -0500

    remove _Room class

--- nanome/_internal/__init__.py
+++ nanome/_internal/__init__.py
@@ -12,5 +12,4 @@ from . import logs
 from .addon import _Addon  # noqa F401
 from .plugin_instance import _PluginInstance  # noqa F401
 from . import plugin_instance_deprecated  # noqa F401
-from .room import _Room  # noqa F401
 from .plugin import _Plugin  # noqa F401
--- nanome/api/room.py
+++ nanome/api/room.py
@@ -1,15 +1,18 @@
-import nanome
-from nanome._internal.room import _Room
+from nanome.util import enums
+from nanome._internal.network import PluginNetwork
+from nanome._internal.network.commands.enums import Messages
 
-
-class Room(_Room):
+class Room:
     """
     | Represents a room in Nanome
     """
-    SkyBoxes = nanome.util.enums.SkyBoxes
+    SkyBoxes = enums.SkyBoxes
 
     def __init__(self):
         self._skybox = Room.SkyBoxes.Unknown
 
     def set_skybox(self, skybox):
         self._set_skybox(skybox)
+
+    def _set_skybox(self, skybox):
+        PluginNetwork._send(Messages.set_skybox, skybox, False)
