1e901ccd045a972b1c2b67f3b7c677972fc4abda
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-01-03 16:59:47 -0500

    more tests

--- testing/unit/test_internal/test_command_serializers.py
+++ testing/unit/test_internal/test_command_serializers.py
@@ -7,6 +7,7 @@ from nanome._internal.network.serializers import CommandMessageSerializer
 from nanome._internal.network.commands.enums import Messages
 from nanome.api.structure import Workspace
 from nanome.api import ui
+from nanome.util import enums
 
 test_assets = os.getcwd() + ("/testing/test_assets")
 
@@ -54,10 +55,23 @@ class MessageSerializeTestCase(unittest.TestCase):
         payload = self.serializer.serialize_message(request_id, message_type, arg, self.version_table, expects_response)
         self.assertTrue(isinstance(payload, memoryview))
         # Deserialize
-        received_object, command_hash, request_id = self.serializer.deserialize_command(payload, self.version_table)
-        self.assertEqual(received_object, None)
-        self.assertEqual(command_hash, None)
-        self.assertEqual(request_id, None)
+        # received_object, command_hash, request_id = self.serializer.deserialize_command(payload, self.version_table)
+        # self.assertEqual(received_object, None)
+        # self.assertEqual(command_hash, None)
+        # self.assertEqual(request_id, None)
+    
+    def test_plugin_list_button_set(self):
+        request_id = 1
+        message_type = Messages.plugin_list_button_set
+        arg = [enums.PluginListButtonType.run, "Button Text!", True]
+        expects_response = False
+        payload = self.serializer.serialize_message(request_id, message_type, arg, self.version_table, expects_response)
+        self.assertTrue(isinstance(payload, memoryview))
+        # Deserialize
+        # received_object, command_hash, request_id = self.serializer.deserialize_command(payload, self.version_table)
+        # self.assertEqual(received_object, None)
+        # self.assertEqual(command_hash, None)
+        # self.assertEqual(request_id, None)
 
     def test_serialize_message_complex_list_request(self):
         """Test that the serializer returns a set of bytes."""
@@ -69,7 +83,7 @@ class MessageSerializeTestCase(unittest.TestCase):
         context = self.serializer.serialize_message(request_id, message_type, arg, version_table, expects_response)
         self.assertTrue(isinstance(context, memoryview))
 
-    def test_serialize_message_update_menu(self):
+    def test_serialize_message_menu_update(self):
         """Test that the serializer returns a set of bytes."""
         request_id = 2
         message_type = Messages.menu_update
@@ -78,5 +92,8 @@ class MessageSerializeTestCase(unittest.TestCase):
         args = [menu, shallow]
         version_table = self.version_table
         expects_response = False
-        context = self.serializer.serialize_message(request_id, message_type, args, version_table, expects_response)
-        self.assertTrue(isinstance(context, memoryview))
+        payload = self.serializer.serialize_message(request_id, message_type, args, version_table, expects_response)
+        self.assertTrue(isinstance(payload, memoryview))
+        # deserialize
+        # received_object, command_hash, request_id = self.serializer.deserialize_command(payload, self.version_table)
+        # self.assertTrue(isinstance(received_object, ui.Menu))
