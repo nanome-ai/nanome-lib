f7ffd115091f27337a1c0344c0322f53fb1e7cea
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-01-11 14:57:47 -0500

    remove shapes internal folder

---
+++ nanome/api/shapes/shape_deprecated.py
@@ -0,0 +1,42 @@
+from nanome._internal.util.decorators import deprecated
+from . import Shape
+
+@property
+@deprecated()
+def target(self):
+    return self._anchors[0].target
+
+
+@target.setter
+@deprecated()
+def target(self, value):
+    self._anchors[0].target = value
+
+
+@property
+@deprecated()
+def anchor(self):
+    return self._anchors[0].anchor_type
+
+
+@anchor.setter
+@deprecated()
+def anchor(self, value):
+    self._anchors[0].anchor_type = value
+
+
+@property
+@deprecated()
+def position(self):
+    return self._anchors[0].local_offset
+
+
+@position.setter
+@deprecated()
+def position(self, value):
+    self._anchors[0].local_offset = value
+
+
+Shape.target = target
+Shape.anchor = anchor
+Shape.position = position
