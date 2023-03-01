46e129f8c2f600641644f9f60229d708a97f5bc1
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-03-01 15:07:44 -0500

    improve tests

--- testing/unit/test_copy.py
+++ testing/unit/test_copy.py
@@ -15,6 +15,16 @@ class CopyTestCase(unittest.TestCase):
     def setUpClass(cls):
         pdb_path = os.path.join(test_assets, 'pdb', '3mcf.pdb')
         cls.complex = struct.Complex.io.from_pdb(path=pdb_path)
+        # Add a random bond to test with.
+        atom1 = next(cls.complex.atoms)
+        atom2 = next(cls.complex.atoms)
+        residue = next(cls.complex.residues)
+        bond = struct.Bond()
+        bond.atom1 = atom1
+        bond.atom2 = atom2
+        bond.kind = util.enums.Kind.CovalentSingle
+        residue.add_bond(bond)
+        # These are the fields we want to validate are copied between structs.
         cls.atom_fields = [
             'index', 'selected', 'labeled', 'atom_rendering', 'surface_rendering',
             'exists', 'is_het', 'occupancy','bfactor','acceptor','donor','polar_hydrogen',
@@ -68,6 +78,9 @@ class CopyTestCase(unittest.TestCase):
         # Assert copy is a different object in memory
         self.assertTrue(atom_copy is not atom)
         self.validate_fields(atom, atom_copy, self.atom_fields)
+        # Assert no residue copied
+        self.assertTrue(atom.residue is not None)
+        self.assertTrue(atom_copy.residue is None)
     
     def test_shallow_copy_residue(self):
         struc = next(self.complex.residues)
@@ -76,36 +89,38 @@ class CopyTestCase(unittest.TestCase):
         # Assert copy is a different object in memory
         self.assertTrue(struc_copy is not struc)
         self.validate_fields(struc, struc_copy, field_list)
+        # Validate no atoms or bonds copied
+        self.assertTrue(len(list(struc.atoms)) > 0)
+        self.assertTrue(len(list(struc_copy.atoms)) == 0)
+        self.assertTrue(len(list(struc.bonds)) > 0)
+        self.assertTrue(len(list(struc_copy.bonds)) == 0)
     
-    @unittest.skip("Find test file with bonds")
     def test_shallow_copy_bonds(self):
         struc = next(self.complex.bonds)
         field_list = self.bond_fields
         struc_copy = struc._shallow_copy()
-        # Assert copy is a different object in memory
         self.assertTrue(struc_copy is not struc)
         self.validate_fields(struc, struc_copy, field_list)
-    
+
     def test_shallow_copy_chain(self):
         struc = next(self.complex.chains)
         field_list = self.chain_fields
         struc_copy = struc._shallow_copy()
         self.assertTrue(struc_copy is not struc)
         self.validate_fields(struc, struc_copy, field_list)
-    
+        # Validate no residues copied
+        self.assertTrue(len(list(struc.residues)) > 0)
+        self.assertTrue(len(list(struc_copy.residues)) == 0)
+
     def test_shallow_copy_molecule(self):
         struc = next(self.complex.molecules)
         field_list = self.molecule_fields
         struc_copy = struc._shallow_copy()
         self.assertTrue(struc_copy is not struc)
         self.validate_fields(struc, struc_copy, field_list)
-    
-    def test_shallow_copy_molecule(self):
-        struc = next(self.complex.molecules)
-        field_list = self.molecule_fields
-        struc_copy = struc._shallow_copy()
-        self.assertTrue(struc_copy is not struc)
-        self.validate_fields(struc, struc_copy, field_list)
+        # Validate no chains copied
+        self.assertTrue(len(list(struc.chains)) > 0)
+        self.assertTrue(len(list(struc_copy.chains)) == 0)
     
     def test_shallow_copy_complex(self):
         struc = self.complex
@@ -113,6 +128,8 @@ class CopyTestCase(unittest.TestCase):
         struc_copy = struc._shallow_copy()
         self.assertTrue(struc_copy is not struc)
         self.validate_fields(struc, struc_copy, field_list)
+        # Validate no molecules copied
+        self.assertTrue(len(list(struc_copy.molecules)) == 0)
 
     def test_deep_copy_complex(self):
         struc = self.complex
@@ -123,18 +140,15 @@ class CopyTestCase(unittest.TestCase):
         for mol, mol_copy in zip(struc.molecules, struc_copy.molecules):
             self.assertTrue(mol is not mol_copy)
             self.validate_fields(mol, mol_copy, self.molecule_fields)
+            self.assertTrue(len(list(mol.chains)) > 0)
             for chain, chain_copy in zip(mol.chains, mol_copy.chains):
                 self.assertTrue(chain is not chain_copy)
                 self.validate_fields(chain, chain_copy, self.chain_fields)
+                self.assertTrue(sum(1 for _ in chain.residues) > 0)
                 for residue, residue_copy in zip(chain.residues, chain_copy.residues):
                     self.assertTrue(residue is not residue_copy)
                     self.validate_fields(residue, residue_copy, self.residue_fields)
+                    self.assertTrue(sum(1 for _ in residue.atoms) > 0)
                     for atom, atom_copy in zip(residue.atoms, residue_copy.atoms):
                         self.assertTrue(atom is not atom_copy)
                         self.validate_fields(atom, atom_copy, self.atom_fields)
-
-    # def test_deep(self):
-    #     deep_copy_tester(2)
-    #     deep_copy_tester(3)
-    #     deep_copy_tester(4)
-    #     deep_copy_tester(5)
