2b63b735372975de4be4482c4a8bd65010e4b281
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-03-07 15:53:03 -0500

    remove unused logger

--- nanome/api/schemas/api_definitions.py
+++ nanome/api/schemas/api_definitions.py
@@ -1,10 +1,6 @@
-import logging
 from nanome.api import structure, schemas
 from nanome.util import enums
 from marshmallow import fields
-
-
-logging = logging.getLogger(__name__)
 
 
 structure_schema_map = {
