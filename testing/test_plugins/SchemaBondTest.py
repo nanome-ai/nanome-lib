2ead76fcd690261936f3e959acbd37b880ac6d57
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-07-10 12:48:20 -0400

    confirm atom count stays the same

--- testing/test_plugins/SchemaBondTest.py
+++ testing/test_plugins/SchemaBondTest.py
@@ -21,11 +21,14 @@ class SchemaBondTest(nanome.AsyncPluginInstance):
         index = shallow[0].index
 
         [comp] = await self.request_complexes([index])
+        atom_count = len(list(comp.atoms))
         bond_count = len(list(comp.bonds))
         comp_data = json.dumps(schemas.ComplexSchema().dump(comp))
         new_comp = schemas.ComplexSchema().loads(comp_data)
+        new_comp_atom_count = len(list(new_comp.atoms))
         new_comp_bond_count = len(list(new_comp.bonds))
         assert bond_count == new_comp_bond_count
+        assert atom_count == new_comp_atom_count
 
         for res in new_comp.residues:
             res.ribbon_color = Color.Blue()
@@ -33,8 +36,10 @@ class SchemaBondTest(nanome.AsyncPluginInstance):
 
         [updated_comp] = await self.request_complexes([index])
         updated_bond_count = len(list(updated_comp.bonds))
+        updated_atom_count = len(list(updated_comp.atoms))
         # this assertion fails
         assert bond_count == updated_bond_count
+        assert atom_count == updated_atom_count
         Logs.message('done')
 
 
