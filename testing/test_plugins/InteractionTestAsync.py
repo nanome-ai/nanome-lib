51cca2979f9439ae5f2c720a405e487981aaae7f
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-07-19 15:37:20 -0400

    add AsyncInteractionsTest

---
+++ testing/test_plugins/InteractionTestAsync.py
@@ -0,0 +1,43 @@
+import nanome
+from nanome.util import enums, async_callback, Color, Logs
+from nanome.api.interactions.interaction import Interaction
+import random
+
+# Config
+
+NAME = "Test Interactions"
+DESCRIPTION = "A simple plugin demonstrating how plugin system can be used to extend Nanome capabilities"
+CATEGORY = "Simple Actions"
+HAS_ADVANCED_OPTIONS = False
+
+# Plugin
+
+
+class InteractionTest(nanome.AsyncPluginInstance):
+
+    def start(self):
+        pass
+
+    @async_callback
+    async def on_run(self):
+        Logs.debug("Getting interactions")
+        interactions = await Interaction.get()
+        if len(interactions) > 0:
+            Logs.debug("Destroy interactions")
+            Interaction.destroy_multiple(interactions)
+        Logs.debug("Request workspace")
+        workspace = await self.request_workspace()
+        # Draw an interaction between two random atoms.
+        [atom1, atom2] = random.sample(list(self.iter(workspace)), 2)
+        interaction = Interaction(enums.InteractionKind.HydrogenBond, Color.Blue(), atom1.index, atom2.index)
+        Logs.debug("Upload interaction")
+        await interaction.upload()
+        Logs.debug("Done")
+
+    def iter(self, workspace):
+        for complex in workspace.complexes:
+            for atom in complex.atoms:
+                yield atom
+
+
+nanome.Plugin.setup(NAME, DESCRIPTION, CATEGORY, HAS_ADVANCED_OPTIONS, InteractionTest)
